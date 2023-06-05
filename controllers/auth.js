a88d037e89481bab31acebfb4619fa47fe992495
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-06-05 20:32:54 +0300

    final changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,9 +9,9 @@ const { User } = require("../models/user");
 
 const { HttpError, cntrlWrapper } = require("../helpers");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
-const avatarsDir = path.join(__dirname, "../", "public", "avatars")
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -22,17 +22,21 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
-  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL});
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({
     user: {
       email: newUser.email,
       subscription: newUser.subscription,
-    }
+    },
   });
 };
 
 const login = async (req, res) => {
-  const { email, password} = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
     throw HttpError(401, "Email or password is wrong");
@@ -41,63 +45,63 @@ const login = async (req, res) => {
   if (!passwordCompare) {
     throw HttpError(401, "Email or password is wrong");
   }
-  
 
   const payload = {
     id: user._id,
-  }
+  };
 
-  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-   
-  await User.findByIdAndUpdate(user._id, {token})
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  await User.findByIdAndUpdate(user._id, { token });
   res.json({
-    token, 
+    token,
     user: {
       email,
       subscription: user.subscription,
-    }  
+    },
   });
- 
 };
 
 const getCurrent = async (req, res) => {
-  const {email, subscription} = req.user;
+  const { email, subscription } = req.user;
   console.log(res.user);
   res.json({
-    email, 
+    email,
     subscription,
-  })
-}
-
-const logout = async (req, res) => {
-  const {_id} = req.user;
-  await User.findByIdAndUpdate(_id, {token: ""});
-  res.status(204).json({});
-}
-
-
-const updateAvatar = async (req, res) => {
-  const { _id} = req.user;	 
-  const { path: tempUpload, originalname } = req.file;	  
-  const avatarName = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarsDir, avatarName);
-  await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", filename);
-  Jimp.read(`${resultUpload}`, (error, image) => {
-    if (error) throw error;
-    image.resize(250, 250).quality(100).write(`${resultUploud}`);
-  });
-  await User.findByIdAndUpdate(_id, { avatarURL });
-  res.status(200).json({
-    avatarURL,
   });
 };
 
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).json({});
+};
+
+const updateAvatar = async (req, res) => {
+  const { _id: userId } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const avatarName = `${userId}_${originalname}`;
+  const resultUpload = path.join(avatarDir, avatarName);
+
+  Jimp.read(tempUpload, (error, image) => {
+    if (error) throw error;
+    image.resize(250, 250).quality(100).write(resultUpload);
+  });
+  fs.unlink(tempUpload);
+
+  const avatar = `avatars/${avatarName}`;
+
+  await User.findByIdAndUpdate(userId, { avatarURL: avatar });
+
+  res.status(200).json({
+    avatarURL: avatar,
+  });
+};
 
 module.exports = {
   register: cntrlWrapper(register),
   login: cntrlWrapper(login),
   getCurrent: cntrlWrapper(getCurrent),
   logout: cntrlWrapper(logout),
-  updateAvatar: cntrlWrapper(updateAvatar)
+  updateAvatar: cntrlWrapper(updateAvatar),
 };
